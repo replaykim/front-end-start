361c732cede38563da96a592b90cf7c614b1b46f
replaykim <replaykim123@gmail.com>
2016-03-10 17:23:20 +0900

    day2

---
+++ 02/app.js
@@ -0,0 +1,33 @@
+// var str = 'hello world!';
+// console.log(str);
+// var nick = 'my nick is \'aji\'';
+
+
+var man = {
+	name: 'replay',
+	age:25,
+	hobby:'bike'
+};
+
+// for(var count=0;count<5;count++){
+
+// 	console.log(count);
+// }
+
+function sum(x){
+	var y=1;
+	return x+y;
+}
+
+//console.log();
+
+console.log(sum(10));
+// if (man.age>30) {
+// 	console.log("old");
+// }
+// else if(man.age>20){
+// 	console.log("young");
+// }
+// else{
+// 	console.log("baby");
+// }
