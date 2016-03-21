21d850334f22b0365041c8e92014dd5bd5ec56a9
replaykim <replaykim123@gmail.com>
2016-03-21 20:50:02 +0900

    chess edit

--- 03/chessborad/app.js
+++ 03/chessborad/app.js
@@ -1,47 +1,27 @@
-
-
-
 var board = document.getElementById("board");
-var size = prompt("얼마나 큰 체스판을 원하시나요")
+var size = 4;
 
+var unClick = null;
+var beforeColor = null;
 function makeChess(tablesize){
-board.style.height=tablesize*100+'px';
-board.style.width=tablesize*100+'px';
-  var spantable = new Array();
+    board.style.height=tablesize*100+'px';
+    board.style.width=tablesize*100+'px';
 
     for(var i=0; i<tablesize; i++){
         for(var j=0; j<tablesize; j++){
-            // TODO : 여기에 필요한 코드를 작성해주세요.
-            console.log("1")
-           
-             spantable[i] = new Array();
-
             var span = document.createElement('span');
-            spantable[i][j]= span;
-            spantable[i][j].className = "black";
-  			
-
-            if (i%2===0) {
-            	if (j%2===0) {
-            		spantable[i][j].className="white";
-            	}
+            if ((i+j)%2===1) {
+                span.className = "black";
             }
-            else  {
-            	if (j%2===1) {
-            		spantable[i][j].className="white";
-            	}
+            else{
+                span.className = "white";
             }
-         	document.getElementById("board").appendChild(span);	
-
-
-         
+            document.getElementById("board").appendChild(span);
         }
     }
-
 }
 
-    var unClick = null;
-    var beforeColor = null;
+
 
     document.getElementById("board").addEventListener("click", function() {
 
